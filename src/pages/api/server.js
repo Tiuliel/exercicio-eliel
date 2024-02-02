8fdbc3f82bbd41e9af7e8cef421cf73c364f2f65
Eliel Moura <124813479+Tiuliel@users.noreply.github.com>
2024-02-02 11:55:36 -0300

    finalizando

--- src/pages/api/server.js
+++ src/pages/api/server.js
@@ -1,2 +1,2 @@
-const serverApi = `https://newsapi.org/v2/everything?q=tesla&from=2024-01-02&sortBy=publishedAt&apiKey=0f0f292679f84590b7d7167fe4269a77`;
+const serverApi = `https://jsonplaceholder.typicode.com/posts`;
 export default serverApi;
