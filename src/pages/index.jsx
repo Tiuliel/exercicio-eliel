b2419f92884211058e3eaa78b99a7378ef01301d
Eliel Moura <124813479+Tiuliel@users.noreply.github.com>
2024-02-02 11:50:52 -0300

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,43 @@
 import Head from "next/head";
 import styled from "styled-components";
+import serverApi from "./api/server";
 
-export default function Home() {
+export async function getStaticProps() {
+  try {
+    const resposta = await fetch(`${serverApi}`);
+    const dados = await resposta.json();
+
+    if (!resposta.ok) {
+      throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
+    }
+
+    return {
+      props: { dados },
+    };
+  } catch (error) {
+    console.error("Deu ruim: " + error.message);
+    return {
+      notFound: true,
+    };
+  }
+}
+
+export default function Home({ dados }) {
+  const teste = dados;
   return (
     <>
       <Head>
         <title>Blog do @TiuLiel</title>
         <meta name="description" content="Web App blog criado com Next.js" />
-        <meta name="keywords" content="Filmes para todo tipo de gente" />
+        <meta name="keywords" content="noticia para todo tipo de gente" />
       </Head>
       <StyledHome>
         <h1>Notícias recentes</h1>
+        {teste.map((noticia) => (
+          <div key={noticia.id}>
+            <p>{noticia.title}</p>
+          </div>
+        ))}
       </StyledHome>
     </>
   );
